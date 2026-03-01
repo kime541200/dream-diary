4c859f701a6163d82673349875ac854099d133aa
kime541200 <kime541200@gmail.com>
2026-03-01 17:28:40 +0800

    chore: install poster export dependencies and init export service

---
+++ src/services/exportService.js
@@ -0,0 +1,6 @@
+// 夢境藝術海報生成服務預研
+export const generateDreamPoster = async (dream) => {
+  // 預計使用 react-native-view-shot 或 canvas 渲染極簡美學海報
+  console.log('Generating poster for:', dream.id);
+  return true;
+};
